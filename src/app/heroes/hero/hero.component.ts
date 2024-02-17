8ca7a5bc8529ba0ac194a91ed23e1e472103add8
Pablo Alejandro Sangenis <pasangenis@teco.com.ar>
2024-02-17 00:00:41 -0300

    Aprendiendo mas sobre los componentes

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
+  public name: string = 'Ironman';
+  public age: number = 45;
 
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescriptionHero(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeNameHero(): void {
+    this.name = 'Superman';
+  }
+
+  changeAgeHero(): void {
+    this.age = 54;
+  }
 }
--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
-
+  public heroNames: string[] = ['Spiderman', 'Superman', 'Thor'];
 }
