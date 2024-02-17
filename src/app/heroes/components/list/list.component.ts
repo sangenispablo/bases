2f07f5453f326007ba32c9f9ade519e677c917f1
Pablo Sangenis <sangenispablo@gmail.com>
2024-02-17 16:02:56 -0300

    Fin de la seccion 4

---
+++ src/app/heroes/components/list/list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  public heroNames: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'She Hulk',
+    'Thor',
+  ];
+
+  public heroRemoved?: string;
+
+  removeLastHero(): void {
+    this.heroRemoved = this.heroNames.pop();
+  }
+}
