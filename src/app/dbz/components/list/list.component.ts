c4bf15affa5a438e73e7c059d1a3e4d56def152a
Pablo Alejandro Sangenis <pasangenis@teco.com.ar>
2024-02-23 18:04:31 -0300

    Emitter

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -7,7 +13,13 @@ import { Character } from '../../interfaces/character.interface';
   styleUrl: './list.component.css',
 })
 export class ListComponent {
-
   @Input()
   public characterList: Character[] = [];
+
+  @Output()
+  public onDeleteIndex: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index: number): void {
+    this.onDeleteIndex.emit(index);
+  }
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -18,111 +18,19 @@ export class MainPageComponent {
     },
     {
       name: 'Broly',
-      power: 10000,
+      power: 1000,
     },
     {
       name: 'Gogeta',
       power: 5000,
     },
-    {
-      name: 'Vegetto',
-      power: 20000,
-    },
-    {
-      name: 'Jiren',
-      power: 20000,
-    },
-    {
-      name: 'Gohan',
-      power: 1000,
-    },
-    {
-      name: 'Piccolo',
-      power: 300,
-    },
-    {
-      name: 'Freezer',
-      power: 53000,
-    },
-    {
-      name: 'Cell',
-      power: 3000,
-    },
-    {
-      name: 'Majin Buu',
-      power: 250000,
-    },
-    {
-      name: 'Bills',
-      power: 10000,
-    },
-    {
-      name: 'Champa',
-      power: 95000,
-    },
-    {
-      name: 'Zamasu',
-      power: 1000,
-    },
-    {
-      name: 'Goku Black',
-      power: 5000,
-    },
-    {
-      name: 'Hit',
-      power: 100,
-    },
-    {
-      name: 'Vados',
-      power: 1000,
-    },
-    {
-      name: 'Whis',
-      power: 1000,
-    },
-    {
-      name: 'Krillin',
-      power: 200,
-    },
-    {
-      name: 'Ten Shin Han',
-      power: 180,
-    },
-    {
-      name: 'Yamcha',
-      power: 140,
-    },
-    {
-      name: 'Bulma',
-      power: 5,
-    },
-    {
-      name: 'Oolong',
-      power: 10,
-    },
-    {
-      name: 'Puar',
-      power: 10,
-    },
-    {
-      name: 'Maestro Roshi',
-      power: 130,
-    },
-    {
-      name: 'Yajirobe',
-      power: 180,
-    },
-    {
-      name: 'Chi-Chi',
-      power: 130,
-    },
-    {
-      name: 'Dende',
-      power: 20,
-    },
-    {
-      name: 'Mr. Popo',
-      power: 1000,
-    },
   ];
+
+  onDeleteIndex(index:number): void {
+    this.characters.splice(index, 1);
+  }
+
+  onNewCharacter(character: Character): void {
+    this.characters.push(character);
+  }
 }
